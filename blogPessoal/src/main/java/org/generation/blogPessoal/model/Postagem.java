be62f97641faa2ba221615e97c3eb6c6237510e0
Carolina Mira <carolinamira@users.noreply.github.com>
2020-09-04 14:26:04 -0300

    version 2

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -5,11 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagem")
@@ -33,6 +36,11 @@ public class Postagem {
 	public long getId() {
 		return id;
 	}
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public void setId(long id) {
 		this.id = id;
 	}
@@ -53,6 +61,13 @@ public class Postagem {
 	}
 	public void setDate(Date date) {
 		this.date = date;
+	}
+	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 }
